1bbac5a80227719ac51f7493877d431814e2f185
jaures <jaures@users.noreply.github.com>
2018-06-21 18:01:04 -0700

    Added vbGuest.sh

---
+++ vbGuest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo deb http://ftp.debian.org/debian stretch-backports main contrib > /etc/apt/sources.list.d/stretch-backports.list
+apt update
+apt install virtualbox-guest-dkms virtualbox-guest-x11 linux-headers-$(uname -r)
